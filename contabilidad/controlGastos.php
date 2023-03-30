792561ef1beb7fbfc4d11374b8251a2245b034bf
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-03-30 08:46:19 -0300

    2

--- contabilidad/controlGastos.php
+++ contabilidad/controlGastos.php
@@ -171,7 +171,9 @@ $hasta = isset($_GET['hasta']) ? $_GET['hasta'] : date("Y-m-d");
             ?>
             <tr>
                 <td><?=  substr($key->FECHA->date, 0, 10); ?></td>
+
                 <td><select id="selectCentroCosto" onchange="cambiarCentroCosto(this)">
+                <option selected><?=  $key->COD_AUXILIAR ?></option>
                 <?php 
                     foreach ($todosLosCentrosCosto as  $y => $centro) {
                 ?>
