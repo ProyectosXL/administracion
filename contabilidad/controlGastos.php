a23dd2f9a19d48fc8621cbaad98aa957bbdf4a45
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-06-28 16:03:58 -0300

    1

--- contabilidad/controlGastos.php
+++ contabilidad/controlGastos.php
@@ -293,6 +293,7 @@ $periodo = str_replace("0","",substr($hasta, 5, 2)).'-'.substr($hasta, 0, 4);
     }
     ?>
 
+
     <script src="js/functions.js"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
@@ -322,7 +323,7 @@ $periodo = str_replace("0","",substr($hasta, 5, 2)).'-'.substr($hasta, 0, 4);
 
             $('.codRubro').select2();
 
-            document.querySelector(".estado").selectedIndex = <?= $_GET['estado'] ? $_GET['estado'] : "" ?>
+            document.querySelector(".estado").selectedIndex = "<?= $_GET['estado'] ? $_GET['estado'] : "" ?>"
             
         });
 
