8637905c445edf353d3fc1946441edf1a655d1d1
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-06-28 11:51:58 -0300

    2

--- contabilidad/coeficientesAjuste.php
+++ contabilidad/coeficientesAjuste.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Articulos sin costo de nacionalizació2n</title>
+  <title>Coeficiente De Ajuste</title>
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
   <!-- <link rel="icon" type="image/jpg" href="images/LOGO XL 2018.jpg"> -->
@@ -20,7 +20,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" id="exampleModalLabel"><i class="fa fa-edit" aria-hidden="true" style="font-size: 25px;"></i> Articulos sin costo de nacionalización</h4>
+                    <h4 class="modal-title" id="exampleModalLabel"><i class="fa fa-edit" aria-hidden="true" style="font-size: 25px;"></i> Coeficiente De Ajuste </h4>
                 </div>
                 <div class="modal-body">
 
