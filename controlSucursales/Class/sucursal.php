f27d6dd4181cac7cbb0c7df831bdba2ccdc0e554
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-08-16 15:48:50 -0300

    1

--- controlSucursales/Class/sucursal.php
+++ controlSucursales/Class/sucursal.php
@@ -164,9 +164,10 @@ class Sucursal
 
         try {
 
-            $sql = "SELECT a.*,b.FACTURA, b.CONTROL , b.RECIBIDO, b.FECHA_RECIBIDO
+            $sql = "SELECT a.*,b.FACTURA, b.CONTROL , b.RECIBIDO, b.FECHA_RECIBIDO, (case when c.FECHA_GUARDADO is not null then 1 else 0 end) guardado
             FROM [LAKERBIS].locales_lakers.dbo.RO_V_GASTOS_CAJA_SUCURSALES a
             left join RO_T_GASTOS_CAJA_SUCURSALES b on  REPLACE(a.N_COMP, ' ', '') = REPLACE (b.N_COMP, ' ', '')  collate Latin1_General_BIN 
+            left join SJ_EGRESOS_DE_CAJA_GUARDADO c on a.n_comp = c.n_comp COLLATE Latin1_General_BIN
             WHERE a.FECHA BETWEEN '$desde' AND '$hasta' AND a.NRO_SUCURS = $sucursal 
             ORDER BY FECHA ASC; 
             ";
--- controlSucursales/controlEgresosCajaSucursales.php
+++ controlSucursales/controlEgresosCajaSucursales.php
@@ -92,6 +92,9 @@
                             </div>
 
                         </form>
+                        
+                        <div id="carruselImagenes" class="modal fade" tabindex="-1" aria-hidden="true" style="margin-left:10%;max-width:80%"></div>
+
             
                         <table class="table table-striped table-bordered" id="myTable" cellspacing="0" data-page-length="100">
                             <thead class="thead-dark" >
@@ -105,6 +108,7 @@
                                     <th > CUENTA </th>
                                     <th > MONTO </th>
                                     <th > LEYENDA </th>
+                                    <th > VER </th>
                                     <th > RECIBIDO </th>
                                     <th > FACTURA </th>
                                     <th > CONTROL </th>
@@ -131,6 +135,16 @@
                                             <td id="td_myTable" ><?= $gasto['DESC_CUENTA'] ?></td>
                                             <td id="td_myTable" >$<?= number_format($gasto['MONTO'], 0, ',', '.') ?></td>
                                             <td id="td_myTable" ><?= $gasto['LEYENDA'] ?></td>
+
+                                            <td id="td_myTable" >
+                                                <?php if($gasto['guardado'] == 1) { ?>
+
+                                                <button class="btn btn-warning" style="margin-left:5px; padding:.3rem .5rem;"  onclick="mostrarImagen(this)">
+                                                    <i class="bi bi-eye" style="color:white"></i>
+                                                </button>
+
+                                                <?php } ?>
+                                            </td>
                                             <td id="td_myTable" <?= $fechaRecibido ?> >
                                                 <?php 
                                                     if($gasto['RECIBIDO'] == 1){
