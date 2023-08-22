dedf8f38048e3b9dc0c468bfb1cddb33727a76e7
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-08-22 09:21:58 -0300

    fix

--- controlSucursales/Class/sucursal.php
+++ controlSucursales/Class/sucursal.php
@@ -164,14 +164,15 @@ class Sucursal
         
         try {
 
-            $sql = "SELECT a.*,b.FACTURA, b.CONTROL , b.RECIBIDO, b.FECHA_RECIBIDO
-            FROM [LAKERBIS].locales_lakers.dbo.RO_V_GASTOS_CAJA_SUCURSALES a
-            left join RO_T_GASTOS_CAJA_SUCURSALES b on  REPLACE(a.N_COMP, ' ', '') = REPLACE (b.N_COMP, ' ', '')  collate Latin1_General_BIN  
-            AND A.NRO_SUCURS = B.NRO_SUCURSAL 
-            AND A.COD_COMP = B.TIPO_COMP collate Latin1_General_BIN 
-            AND A.COD_CTA = B.COD_CUENTA
+            $sql = "SELECT a.*,b.FACTURA, b.CONTROL , b.RECIBIDO, b.FECHA_RECIBIDO, 
+            (case when c.FECHA_GUARDADO is not null then 1 else 0 end) guardado
+            FROM [LAKERBIS].locales_lakers.dbo.RO_V_GASTOS_CAJA_SUCURSALES a 
+            left join RO_T_GASTOS_CAJA_SUCURSALES b on REPLACE(a.N_COMP, ' ', '') = REPLACE (b.N_COMP, ' ', '') collate Latin1_General_BIN 
+            AND A.NRO_SUCURS = B.NRO_SUCURSAL AND A.COD_COMP = B.TIPO_COMP collate Latin1_General_BIN
+            AND A.COD_CTA = B.COD_CUENTA 
+            left join SJ_EGRESOS_DE_CAJA_GUARDADO c on a.N_COMP = c.N_COMP collate Latin1_General_BIN
             WHERE a.FECHA BETWEEN '$desde' AND '$hasta' AND a.NRO_SUCURS = $sucursal 
-            ORDER BY FECHA ASC; 
+            ORDER BY FECHA ASC;
             ";
        
             
