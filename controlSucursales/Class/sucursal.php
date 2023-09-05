64b85b1ac233f04b3683a17c265a8a891fefcec7
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-09-05 16:06:47 -0300

    5/9-controlMasivo where verificado

--- controlSucursales/Class/sucursal.php
+++ controlSucursales/Class/sucursal.php
@@ -71,7 +71,7 @@ class Sucursal
         $sql = "SELECT * FROM  ".$this->cid->prefix."RO_T_VENTA_DIARIA_SUCURSALES where nro_sucursal = '$nroSucursal' 
         AND FECHA BETWEEN '$desde' AND '$hasta' 
         AND MEDIO_PAGO = '$medioDePago' 
-        AND VERIFICADO = '0' ;";
+        // AND VERIFICADO = '0' ;";
 
         $stmt = sqlsrv_query($this->cid_locales, $sql);
 
