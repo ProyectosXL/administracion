422c4c320d649ee25c92ab09dd02fa51005daa5a
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-09-27 10:02:11 -0300

    27/9

--- impuestos/alquileres/Class/Alquiler.php
+++ impuestos/alquileres/Class/Alquiler.php
@@ -582,6 +582,69 @@ class Alquiler
         }
     }
     
+    function traerContratoVigente(){
+
+        $sql="
+        DECLARE @fecha_actual DATE = GETDATE(); -- Obtiene la fecha actual
+        
+        WITH CTE AS (
+            SELECT
+                *,
+                ROW_NUMBER() OVER(PARTITION BY NRO_SUCURS ORDER BY ID DESC) AS rn
+            FROM RO_T_CONTRATOS_ALQUILERES
+            WHERE VIG_DESDE <= @fecha_actual
+              AND VIG_HASTA >= @fecha_actual
+        )
+        
+        SELECT * FROM CTE WHERE rn IN (1)";
+
+        try {
+        
+            $stmt = sqlsrv_query($this->cid_central, $sql);
+    
+            $rows = array();
+    
+            while ($v = sqlsrv_fetch_array($stmt)) {
+                $rows[] = $v;
+            }
+    
+            return $rows;
+        } catch (\Throwable $th) {
+            throw $th; 
+        }
+
+    }
+
+    function traerContratoAnterior () {
+
+        $sql=" DECLARE @fecha_actual DATE = GETDATE(); -- Obtiene la fecha actual
+
+        WITH CTE AS (
+            SELECT
+                *,
+                ROW_NUMBER() OVER(PARTITION BY NRO_SUCURS ORDER BY ABS(DATEDIFF(DAY, VIG_HASTA, @fecha_actual))) AS rn
+            FROM RO_T_CONTRATOS_ALQUILERES
+            WHERE VIG_HASTA <= @fecha_actual
+        )
+        
+        SELECT * FROM CTE WHERE rn IN (1); ";
+
+        try {
+        
+            $stmt = sqlsrv_query($this->cid_central, $sql);
+    
+            $rows = array();
+    
+            while ($v = sqlsrv_fetch_array($stmt)) {
+                $rows[] = $v;
+            }
+    
+            return $rows;
+        } catch (\Throwable $th) {
+            throw $th; 
+        }
+
+    }
 }
 
 
