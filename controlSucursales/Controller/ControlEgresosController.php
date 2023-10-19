3e59da23009303518fdd77d78602f9e45642ae32
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-10-19 08:46:26 -0300

    19/10

--- controlSucursales/Controller/ControlEgresosController.php
+++ controlSucursales/Controller/ControlEgresosController.php
@@ -133,6 +133,7 @@ function contarFotosEnCarpeta() {
     
     $nComp = (isset($_POST['nComp'])) ? $_POST['nComp'] : "";
     $nroSucursal = $_POST['nroSucursal'];
+    $codCuenta = (isset ($_POST['codCuenta'])) ? $_POST['codCuenta'] : "";
     $root = $_SERVER["DOCUMENT_ROOT"];
 
     $targetDir = $root.'/Imagenes/egresosCaja/';
@@ -180,7 +181,7 @@ function contarFotosEnCarpeta() {
     }else{
 
     
-        $fileName = $nComp.$nroSucursal;
+        $fileName = $nComp.$nroSucursal.$codCuenta;
         $contadorFotos = 0;
         $datosDeLosArchivos = [];
         $datosDeLosArchivos['cantidad'] = 0; 
