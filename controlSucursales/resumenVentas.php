3aeb350f1fbbd2348716727ff5e8ccee16d35445
Ramiro Orozco <ramiro.orozco@lakerscorp.global>
2023-10-24 09:09:57 -0300

    cambios front resumen de ventas

--- controlSucursales/resumenVentas.php
+++ controlSucursales/resumenVentas.php
@@ -30,34 +30,23 @@ $hasta = isset($_GET['hasta']) ? $_GET['hasta'] : date("Y-m-d");
 
     <div class="alert alert-secondary">
        
-        <div class="row">
-            <div class="form-row">
-            <h4 class="ml-3 mt-4"><i class="bi bi-credit-card"></i>  Ventas por medio de pago <a style="color: #6c757d;"><?php if (isset($_GET['desde'])){ echo $desde ?> a <?php echo $hasta ;}?></a></h4>
-                <form action="">
-                    <div class="contenedor">
-                        <div class="col-">
-                            <label>Desde:</label>
-                            <input type="date" class="form-control form-control-sm" name="desde" value="<?= $desde ?>">
-                        </div>
-
-                        <div class="ml-2">
-                            <label>Hasta:</label>
-                            <input type="date" class="form-control form-control-sm" name="hasta" value="<?= $hasta ?>">
-                        </div>
-                        <button type="submit" name="submit" class="btn btn-primary" id="search">Buscar <i class="bi bi-search"></i></button>
-                        <button type="submit" name="submit" class="btn btn-success" id="btnExport">Exportar <i class="bi bi-file-earmark-excel"></i></button>
-                        <div class="mt-2" id="busqRapida">
-                            <label id="textBusqueda">Busqueda rapida:</label>
-                            <input type="text" id="textBox" placeholder="Sobre cualquier campo..." onkeyup="myFunction()" class="form-control form-control-sm"></input>
-                        </div>
-                        <!-- spinner -->
-                        <div id="boxLoading"></div> 
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
+    <form class="form-inline mt-3 mb-3">
+            <h4 class="ml-3"><i class="bi bi-credit-card"></i>  Ventas por medio de pago <a style="color: #6c757d;"><?php if (isset($_GET['desde'])){ echo $desde ?> a <?php echo $hasta ;}?></a></h4>
+                <label class="ml-4">Desde:</label>
+                <input type="date" class="form-control form-control-sm ml-1" name="desde" value="<?= $desde ?>">
+                <label class="ml-2">Hasta:</label>
+                <input type="date" class="form-control form-control-sm ml-1" name="hasta" value="<?= $hasta ?>">
+                        
+                <button type="submit" name="submit" class="btn btn-primary ml-2" id="search">Buscar <i class="bi bi-search"></i></button>
+                <button type="submit" name="submit" class="btn btn-success ml-3" id="btnExport">Exportar <i class="bi bi-file-earmark-excel"></i></button>
+                
+                <label id="textBusqueda" class="ml-4">Busqueda rapida:</label>
+                <input type="text" id="textBox" placeholder="Sobre cualquier campo..." onkeyup="myFunction()" class="form-control form-control-sm ml-1"></input>
+                
+                <!-- spinner -->
+                <div id="boxLoading"></div>     
+    </form>
+           
     <?php
 
     if (isset($_GET['desde'])) {
