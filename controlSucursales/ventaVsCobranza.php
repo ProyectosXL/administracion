c1039421eddf88afe0dc5e99dbbca4bfe546b868
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2024-03-04 14:22:06 -0300

    1

--- controlSucursales/ventaVsCobranza.php
+++ controlSucursales/ventaVsCobranza.php
@@ -32,34 +32,38 @@ $hasta = isset($_GET['hasta']) ? $_GET['hasta'] : date("Y-m-d");
 <body>
 
     <div class="alert alert-secondary">
-       
-        <div class="row">
-            <div class="form-row">
-            <h4 class="ml-3 mt-4"><i class="bi bi-card-checklist"></i>  Ventas vs Cobranza por comprobante <a style="color: #6c757d;"><?php if (isset($_GET['desde'])){ echo $desde ?> a <?php echo $hasta ;}?></a></h4>
-                <form action="">
-                    <div class="contenedor">
-                        <div class="col-">
-                            <label>Desde:</label>
-                            <input type="date" class="form-control form-control-sm" name="desde" value="<?= $desde ?>">
-                        </div>
-
-                        <div class="ml-2">
-                            <label>Hasta:</label>
-                            <input type="date" class="form-control form-control-sm" name="hasta" value="<?= $hasta ?>">
-                        </div>
-                        <button type="submit" name="submit" class="btn btn-primary ventaVsCobranza" id="search">Buscar <i class="bi bi-search"></i></button>
-                        <button type="submit" name="submit" class="btn btn-success ventaVsCobranza" id="btnExport">Exportar <i class="bi bi-file-earmark-excel"></i></button>
-                        <div class="mt-2" id="busqRapida">
-                            <label id="textBusqueda">Busqueda rapida:</label>
-                            <input type="text" id="textBox" placeholder="Sobre cualquier campo..." onkeyup="myFunction()" class="form-control form-control-sm"></input>
-                        </div>
-                        <!-- spinner -->
-                        <div id="boxLoading"></div> 
-                    </div>
-                </form>
+        <div style="margin-left:1%">
+            <div class="row">
+                
+                <h4 class="ml-3 mt-4"><i class="bi bi-card-checklist"></i>  Ventas vs Cobranza por comprobante <a style="color: #6c757d;"><?php if (isset($_GET['desde'])){ echo $desde ?> a <?php echo $hasta ;}?></a></h4>
             </div>
+            <div class="row" style='margin-left:1%'>   
+                    <div class="col mt-2">
+                        <form action="">
+                   
+                            <label>Desde:</label>
+                            <input type="date" class="" name="desde" value="<?= $desde ?>">
+                    
+
+                        
+                            <label>Hasta:</label>
+                            <input type="date" class="mr-3" name="hasta" value="<?= $hasta ?>" >
+                  
+                            <button type="submit" name="submit" class="btn btn-primary ventaVsCobranza " id="search">Buscar <i class="bi bi-search"></i></button>
+                            <button type="submit" name="submit" class="btn btn-success ventaVsCobranza " id="btnExport">Exportar <i class="bi bi-file-earmark-excel"></i></button>
+               
+                            <label id="textBusqueda" class="ml-5">Busqueda rapida:</label>
+                            <input type="text" id="textBox" placeholder="Sobre cualquier campo..." onkeyup="myFunction()" class=""></input>
+    
+            
+                            <div id="boxLoading"></div> 
+                        </form>
+                    </div>
+            </div>
+            
         </div>
     </div>
+        
 
     <?php
 
