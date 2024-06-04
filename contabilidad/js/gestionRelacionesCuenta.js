e280b1ba9bf0d0f71f5ec32a1f58136e4bef03f2
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2024-06-04 10:35:32 -0300

    1

--- contabilidad/js/gestionRelacionesCuenta.js
+++ contabilidad/js/gestionRelacionesCuenta.js
@@ -67,3 +67,26 @@ const agregar = () => {
         }
       });
 }
+
+
+const cambiarEntorno = (t) =>{
+
+    let entorno = 0;
+  
+    if(t.getAttribute("data-off") == "ARG" ){
+      entorno = 0;
+    }else{
+      entorno = 1;
+    }
+
+
+    $.ajax({
+      url: "Controller/cambiarEntorno.php",
+      method: "POST",
+      data : {entorno: entorno},
+      success: function (data) {
+        location.reload();
+      }
+    });
+
+  }
