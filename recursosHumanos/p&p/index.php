c777bd9dc303dced39c00c61890117c8556942b8
ProyectosXL <proyectos@xl.com.ar>
2025-08-04 11:45:28 -0300

    cambios rrhh

--- recursosHumanos/p&p/index.php
+++ recursosHumanos/p&p/index.php
@@ -39,6 +39,7 @@ $glosario = $politicaObj->obtenerGlosario();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Gestión de Políticas y Procedimientos</title>
+    <link rel="icon" type="image/jpg" href="../../image/icono.jpg">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="css/main.css">
     <!-- PDF.js -->
