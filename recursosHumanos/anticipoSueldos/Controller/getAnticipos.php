43efe5c4ecc45a3f1c76c20de77d6f6b57a4dc00
SantosgarciaXL <santos.garcia@xl.com.ar>
2026-03-18 14:29:03 -0300

    add filtro Departamento

--- recursosHumanos/anticipoSueldos/Controller/getAnticipos.php
+++ recursosHumanos/anticipoSueldos/Controller/getAnticipos.php
@@ -12,6 +12,7 @@ try {
     $length = isset($_POST['length']) ? intval($_POST['length']) : 10;
     $search = isset($_POST['search']['value']) ? $_POST['search']['value'] : '';
     $periodo = isset($_POST['periodo']) ? $_POST['periodo'] : '';
+    $departamento = isset($_POST['departamento']) ? $_POST['departamento'] : '';
     
     // Parámetros de ordenamiento
     $orderColumnIndex = isset($_POST['order'][0]['column']) ? intval($_POST['order'][0]['column']) : 5;
@@ -21,12 +22,12 @@ try {
     $columns = ['NRO_LEGAJO', 'APELLIDO_Y_NOMBRE', 'DNI', 'PERIODO', 'IMPORTE', 'FECHA_CARGA', 'DESC_DEPARTAMENTO'];
     $orderColumn = isset($columns[$orderColumnIndex]) ? $columns[$orderColumnIndex] : 'FECHA_CARGA';
     
-    error_log("getAnticipos - Start: $start, Length: $length, Search: '$search', Periodo: '$periodo', Order: $orderColumn $orderDir");
+    error_log("getAnticipos - Start: $start, Length: $length, Search: '$search', Periodo: '$periodo', Departamento: '$departamento', Order: $orderColumn $orderDir");
     
     // Obtener datos usando los métodos de la clase
-    $data = $anticipo->obtenerAnticipos($start, $length, $search, $periodo, $orderColumn, $orderDir);
-    $total = $anticipo->contarAnticipos('', $periodo);
-    $filtered = $anticipo->contarAnticipos($search, $periodo);
+    $data = $anticipo->obtenerAnticipos($start, $length, $search, $periodo, $orderColumn, $orderDir, $departamento);
+    $total = $anticipo->contarAnticipos('', $periodo, $departamento);
+    $filtered = $anticipo->contarAnticipos($search, $periodo, $departamento);
     
     error_log("getAnticipos - Total: $total, Filtered: $filtered, Data count: " . count($data));
     
